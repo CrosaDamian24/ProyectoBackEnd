78083be1599f9725d96bf5fb65d847a5f27f3c3c
Crosa Damián <dcrosa@prevencionart.com.ar>
2023-07-25 22:09:10 -0300

    desafio Cookies, Session & Storage

--- src/app.js
+++ src/app.js
@@ -7,6 +7,11 @@ import { Server } from 'socket.io'
 import viewsRouter from './routers/views.router.js'
 import mongoose from 'mongoose';
 import routerChat from "./routers/chat.router.js"
+import sessionRouter from './routers/session.router.js'
+
+import session from "express-session"; //DEPENDENCIA SESSION (guarda cookie)
+import MongoStore from "connect-mongo"; //DEPENDENCIA guardar datos en MONGO
+
 
 const app = express()
 app.use(express.json())
@@ -14,27 +19,55 @@ app.use(express.urlencoded({extended:true}))
 
 
 
+
+  
 //cofiguracion del motor e plantillas handlebars
-app.use(express.static('./public'))
+// app.use(express.static('./public'))
+app.use(express.static(__dirname + "/public"))
 app.engine('handlebars', handlebars.engine())
-app.set('views', './views')
-// app.set('views',__dirname +'/views')
+// app.set('views', './views')
+app.set('views',__dirname +'/views')
 app.set('view engine','handlebars')
 
-app.get('/',(req,res) => res.render('index'))
+// app.get('/',(req,res) => res.render('index'))
+
+app.get('/',(req,res) => res.render('sessions/login'))
+
+
 //productos
+
+
+
+
+const MONGO_URI = "mongodb+srv://coder:coder@cluster0.xho3yyy.mongodb.net/"
+const MONGO_DB_NAME = "ecommerce"
+
+
+// MIDLEWARE CREA SESSION Y GUARDA EN DB MONGO
+app.use(session({ //SESSION ES UN OBJETO
+    store: MongoStore.create({ //ALMACENA EN MONGO
+        mongoUrl: MONGO_URI,
+        dbName: MONGO_DB_NAME,
+
+    }),
+    secret: "palabraclave",
+    resave: true,
+    saveUninitialized: true
+}))
+
+
 app.use('/views',viewsRouter)
 app.use('/api/products',  productsRouter)
 //carts
 app.use('/api/carts',  cartsRouter)
 app.use("/chat", routerChat)
-
+app.use("/session", sessionRouter) //ruta crea session
 
 
 mongoose.set('strictQuery', false)
 try{
     await mongoose.connect('mongodb+srv://coder:coder@cluster0.xho3yyy.mongodb.net/ecommerce')
-
+   
   
 }catch(err){
     console.log(err.message)
@@ -54,3 +87,4 @@ io.on("connection",socket => {
     })
 
 })
+
---
+++ src/routers/session.router.js
@@ -0,0 +1,98 @@
+import { Router } from "express";
+import UserModel from "../dao/models/user.model.js";
+import { createHash, isValidPassword } from "../utils.js";
+
+
+const router = Router()
+
+
+
+//Vista para registrar usuarios
+router.get('/register', (req, res) => {
+    res.render('sessions/register')
+})
+
+// API para crear usuarios en la DB
+router.post('/register', async(req,res)=>{
+    // const userNew = req.body
+    const userNew = {
+        first_name : req.body.first_name,
+        last_name : req.body.last_name,
+        age : req.body.age,
+        email : req.body.email,
+        password : createHash(req.body.password)
+
+    }
+
+    const user = new UserModel(userNew)
+    await user.save()
+
+    res.redirect('/session/login')
+
+})
+
+
+// Vista de Login
+router.get('/login', (req, res) => {
+    res.render('sessions/login')
+})
+
+//API para login
+router.post('/login',async(req,res) =>{
+    const {email,password} = req.body
+
+    const user = await UserModel.findOne({email}).lean().exec()
+    // const user = await UserModel.findOne({email,password}).lean().exec()
+    if(!user  && (email !== 'admin@coderhouse.com' || password !== 'adminCod3r123') ){
+     
+        return res.status(401).render('errors/base',{
+            error:'Error en email y/o contraseña'
+        })   
+   
+    }
+if (email !== 'admin@coderhouse.com' || password !== 'adminCod3r123'){
+    if (!isValidPassword(user,password) ){
+        return res.status(401).render('errors/base',{
+            error:'Error en email y/o contraseña'
+        })   
+    }
+    
+}
+ 
+   if (email !== 'admin@coderhouse.com'){
+    req.session.user = user
+   }else{
+    req.session.user = {
+        // _id: '64bbf76d05d10820794daa20',
+        first_name: 'Admin',
+        // last_name: 'Crosa',
+        // email: 'damiancrosa@hotmail.com',
+        // age: 45,
+        // password: 'secreto',
+        // __v: 0,
+         role: 'User'
+      }
+   }
+
+    // req.session.user = user
+
+  
+     res.redirect('/views/products/')
+
+})
+
+// Cerrar Session
+router.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if(err) {
+            console.log(err);
+            res.status(500).render('errors/base', {error: err})
+        } else res.redirect('/')
+    })
+    
+})
+
+
+
+
+export default router 
